89d4012ee92273c1c0f887d2d3f8821d6c29335b
Julio Dinis <lima.juliolima.julio45@gmail.com>
2023-05-05 09:55:25 -0300

    Listagem de cursos usando arquivo json

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,14 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+import { CoursesModule } from './courses/courses.module';
+
+const routes: Routes = [
+  { path: '', pathMatch: 'full', redirectTo: 'courses' },
+  { path: 'courses',
+  loadChildren: () => import('./courses/courses.module').then( m =>CoursesModule)
+}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
